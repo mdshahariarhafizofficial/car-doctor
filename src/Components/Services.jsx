3c4c0e61378ab13a2f41a0146a35cd4b4049ff96
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-09-27 22:36:25 +0600

    Service Section Done

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -5,31 +5,30 @@ import Image from 'next/image';
 import { ArrowRight } from 'lucide-react';
 
 const Services = async () => {
-    const allServices = await dbConnect("services").find({}).toArray();
-    console.log(allServices);
+    const allServices = await dbConnect("services").find({}).limit(6).toArray();
     
     return (
         <div className='max-w-[1140px] mx-auto py-20 px-5 lg:px-0'>
             <SectionText sectionName={"Service"} Title={"Our Service Area"}></SectionText>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10'>
                 {
                     allServices.map((service, idx) => {
                         return (
                             <div key={idx}>
                                 <div className="card bg-base-100 shadow-sm">
-                                <figure>
+                                <figure className='p-5'>
                                     <Image
-                                    className='w-full aspect-video'
+                                    className='w-full aspect-video rounded-lg'
                                     src={service.img} 
                                     width={500}
                                     height={500}
                                     alt='service'
                                     />
                                 </figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">{service.title}</h2>
+                                <div className="card-body pt-0">
+                                    <h2 className="card-title text-2xl font-bold">{service.title}</h2>
                                     <div className="card-actions justify-between items-center">
-                                    <h3>
+                                    <h3 className='text-lg font-semibold'>
                                         Price: $ {service.price}
                                     </h3>
                                     <button className="">
@@ -43,6 +42,11 @@ const Services = async () => {
                     }
                     )
                 }
+            </div>
+            <div className='flex items-center justify-center py-6'>
+                <button className="btn btn-outline btn-primary px-8 font-bold py-4 rounded-sm transition cursor-pointer">
+                    More Services
+                </button>            
             </div>
         </div>
     );
